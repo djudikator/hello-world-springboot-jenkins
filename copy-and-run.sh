d41b9eb83225505bf19e33e104b567b63bf8f0e8
Vladimir Malyshev <vladimir.malyshev@aurea.com>
2018-05-14 22:35:48 +0300

    add fixes

--- copy-and-run.sh
+++ copy-and-run.sh
@@ -1,2 +1,2 @@
-cp ./target/hello-world-springboot-1.0.0-SNAPSHOT.jar  ./home/app.jar
-java -jar ./home/app.jar
+cp ./target/hello-world-springboot-1.0.0-SNAPSHOT.jar  /home/app.jar
+java -jar /home/app.jar
